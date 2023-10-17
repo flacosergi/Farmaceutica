804cb0c91ee9fb4d6aa7b178404d3a1ed6a3069e
galianosergio <galianosergio@hotmail.com>
2023-10-17 17:40:39 -0300

    sg

---
+++ Presentacion/Articulos.Designer.cs
@@ -0,0 +1,439 @@
+﻿namespace Farmaceutica.Presentacion
+{
+    partial class Articulos
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            button1 = new Button();
+            panel1 = new Panel();
+            button2 = new Button();
+            button4 = new Button();
+            button5 = new Button();
+            panel2 = new Panel();
+            pictureBox1 = new PictureBox();
+            label1 = new Label();
+            label2 = new Label();
+            label3 = new Label();
+            comboBox1 = new ComboBox();
+            label4 = new Label();
+            textBox1 = new TextBox();
+            label5 = new Label();
+            comboBox2 = new ComboBox();
+            comboBox3 = new ComboBox();
+            label6 = new Label();
+            label7 = new Label();
+            maskedTextBox1 = new MaskedTextBox();
+            maskedTextBox2 = new MaskedTextBox();
+            label8 = new Label();
+            maskedTextBox3 = new MaskedTextBox();
+            label9 = new Label();
+            maskedTextBox4 = new MaskedTextBox();
+            label10 = new Label();
+            maskedTextBox5 = new MaskedTextBox();
+            label11 = new Label();
+            button3 = new Button();
+            checkBox1 = new CheckBox();
+            panel1.SuspendLayout();
+            panel2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
+            SuspendLayout();
+            // 
+            // button1
+            // 
+            button1.BackColor = Color.DarkCyan;
+            button1.FlatStyle = FlatStyle.Flat;
+            button1.Font = new Font("Segoe UI", 9F, FontStyle.Bold, GraphicsUnit.Point);
+            button1.ForeColor = SystemColors.Control;
+            button1.Location = new Point(33, 18);
+            button1.Name = "button1";
+            button1.Size = new Size(85, 27);
+            button1.TabIndex = 0;
+            button1.Text = "Nuevo";
+            button1.UseVisualStyleBackColor = false;
+            // 
+            // panel1
+            // 
+            panel1.BackColor = Color.PaleTurquoise;
+            panel1.BorderStyle = BorderStyle.FixedSingle;
+            panel1.Controls.Add(button3);
+            panel1.Controls.Add(button5);
+            panel1.Controls.Add(button4);
+            panel1.Controls.Add(button2);
+            panel1.Controls.Add(button1);
+            panel1.Location = new Point(17, 417);
+            panel1.Name = "panel1";
+            panel1.Size = new Size(801, 71);
+            panel1.TabIndex = 1;
+            // 
+            // button2
+            // 
+            button2.BackColor = Color.DarkCyan;
+            button2.FlatStyle = FlatStyle.Flat;
+            button2.Font = new Font("Segoe UI", 9F, FontStyle.Bold, GraphicsUnit.Point);
+            button2.ForeColor = SystemColors.Control;
+            button2.Location = new Point(143, 18);
+            button2.Name = "button2";
+            button2.Size = new Size(85, 27);
+            button2.TabIndex = 1;
+            button2.Text = "Editar";
+            button2.UseVisualStyleBackColor = false;
+            // 
+            // button4
+            // 
+            button4.BackColor = Color.DarkCyan;
+            button4.FlatStyle = FlatStyle.Flat;
+            button4.Font = new Font("Segoe UI", 9F, FontStyle.Bold, GraphicsUnit.Point);
+            button4.ForeColor = SystemColors.Control;
+            button4.Location = new Point(257, 18);
+            button4.Name = "button4";
+            button4.Size = new Size(85, 27);
+            button4.TabIndex = 2;
+            button4.Text = "Guardar";
+            button4.UseVisualStyleBackColor = false;
+            // 
+            // button5
+            // 
+            button5.BackColor = Color.DarkCyan;
+            button5.FlatStyle = FlatStyle.Flat;
+            button5.Font = new Font("Segoe UI", 9F, FontStyle.Bold, GraphicsUnit.Point);
+            button5.ForeColor = SystemColors.Control;
+            button5.Location = new Point(700, 18);
+            button5.Name = "button5";
+            button5.Size = new Size(85, 27);
+            button5.TabIndex = 4;
+            button5.Text = "Salir";
+            button5.UseVisualStyleBackColor = false;
+            // 
+            // panel2
+            // 
+            panel2.BackColor = Color.White;
+            panel2.BorderStyle = BorderStyle.FixedSingle;
+            panel2.Controls.Add(checkBox1);
+            panel2.Controls.Add(maskedTextBox5);
+            panel2.Controls.Add(label11);
+            panel2.Controls.Add(maskedTextBox4);
+            panel2.Controls.Add(label10);
+            panel2.Controls.Add(maskedTextBox3);
+            panel2.Controls.Add(label9);
+            panel2.Controls.Add(maskedTextBox2);
+            panel2.Controls.Add(label8);
+            panel2.Controls.Add(maskedTextBox1);
+            panel2.Controls.Add(label7);
+            panel2.Controls.Add(label6);
+            panel2.Controls.Add(comboBox3);
+            panel2.Controls.Add(comboBox2);
+            panel2.Controls.Add(label5);
+            panel2.Controls.Add(textBox1);
+            panel2.Controls.Add(label4);
+            panel2.Controls.Add(comboBox1);
+            panel2.Controls.Add(label3);
+            panel2.Controls.Add(label2);
+            panel2.Controls.Add(label1);
+            panel2.Controls.Add(pictureBox1);
+            panel2.Location = new Point(17, 19);
+            panel2.Name = "panel2";
+            panel2.Size = new Size(801, 379);
+            panel2.TabIndex = 0;
+            // 
+            // pictureBox1
+            // 
+            pictureBox1.BorderStyle = BorderStyle.FixedSingle;
+            pictureBox1.Location = new Point(489, 13);
+            pictureBox1.Name = "pictureBox1";
+            pictureBox1.Size = new Size(296, 260);
+            pictureBox1.TabIndex = 0;
+            pictureBox1.TabStop = false;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Segoe UI", 9F, FontStyle.Bold, GraphicsUnit.Point);
+            label1.ForeColor = Color.DarkCyan;
+            label1.Location = new Point(21, 24);
+            label1.Name = "label1";
+            label1.Size = new Size(95, 15);
+            label1.TabIndex = 12;
+            label1.Text = "Codigo Artículo:";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Font = new Font("Segoe UI", 12F, FontStyle.Bold, GraphicsUnit.Point);
+            label2.Location = new Point(143, 19);
+            label2.Name = "label2";
+            label2.Size = new Size(37, 21);
+            label2.TabIndex = 1;
+            label2.Text = "000";
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Font = new Font("Segoe UI", 9F, FontStyle.Bold, GraphicsUnit.Point);
+            label3.ForeColor = Color.DarkCyan;
+            label3.Location = new Point(21, 58);
+            label3.Name = "label3";
+            label3.Size = new Size(81, 15);
+            label3.TabIndex = 13;
+            label3.Text = "Tipo Artículo:";
+            // 
+            // comboBox1
+            // 
+            comboBox1.DropDownStyle = ComboBoxStyle.DropDownList;
+            comboBox1.FormattingEnabled = true;
+            comboBox1.Location = new Point(146, 56);
+            comboBox1.Name = "comboBox1";
+            comboBox1.Size = new Size(164, 23);
+            comboBox1.TabIndex = 2;
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Font = new Font("Segoe UI", 9F, FontStyle.Bold, GraphicsUnit.Point);
+            label4.ForeColor = Color.DarkCyan;
+            label4.Location = new Point(21, 96);
+            label4.Name = "label4";
+            label4.Size = new Size(72, 15);
+            label4.TabIndex = 14;
+            label4.Text = "Descripcón:";
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(146, 93);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(324, 23);
+            textBox1.TabIndex = 3;
+            // 
+            // label5
+            // 
+            label5.AutoSize = true;
+            label5.Font = new Font("Segoe UI", 9F, FontStyle.Bold, GraphicsUnit.Point);
+            label5.ForeColor = Color.DarkCyan;
+            label5.Location = new Point(21, 135);
+            label5.Name = "label5";
+            label5.Size = new Size(100, 15);
+            label5.TabIndex = 15;
+            label5.Text = "Marca / Laborat.:";
+            // 
+            // comboBox2
+            // 
+            comboBox2.DropDownStyle = ComboBoxStyle.DropDownList;
+            comboBox2.FormattingEnabled = true;
+            comboBox2.Location = new Point(146, 132);
+            comboBox2.Name = "comboBox2";
+            comboBox2.Size = new Size(164, 23);
+            comboBox2.TabIndex = 4;
+            // 
+            // comboBox3
+            // 
+            comboBox3.DropDownStyle = ComboBoxStyle.DropDownList;
+            comboBox3.FormattingEnabled = true;
+            comboBox3.Location = new Point(146, 171);
+            comboBox3.Name = "comboBox3";
+            comboBox3.Size = new Size(164, 23);
+            comboBox3.TabIndex = 5;
+            // 
+            // label6
+            // 
+            label6.AutoSize = true;
+            label6.Font = new Font("Segoe UI", 9F, FontStyle.Bold, GraphicsUnit.Point);
+            label6.ForeColor = Color.DarkCyan;
+            label6.Location = new Point(21, 174);
+            label6.Name = "label6";
+            label6.Size = new Size(69, 15);
+            label6.TabIndex = 16;
+            label6.Text = "U. Medida.:";
+            // 
+            // label7
+            // 
+            label7.AutoSize = true;
+            label7.Font = new Font("Segoe UI", 9F, FontStyle.Bold, GraphicsUnit.Point);
+            label7.ForeColor = Color.DarkCyan;
+            label7.Location = new Point(326, 174);
+            label7.Name = "label7";
+            label7.Size = new Size(58, 15);
+            label7.TabIndex = 17;
+            label7.Text = "Cantidad:";
+            // 
+            // maskedTextBox1
+            // 
+            maskedTextBox1.Location = new Point(390, 171);
+            maskedTextBox1.Name = "maskedTextBox1";
+            maskedTextBox1.Size = new Size(80, 23);
+            maskedTextBox1.TabIndex = 6;
+            // 
+            // maskedTextBox2
+            // 
+            maskedTextBox2.Location = new Point(146, 211);
+            maskedTextBox2.Name = "maskedTextBox2";
+            maskedTextBox2.Size = new Size(164, 23);
+            maskedTextBox2.TabIndex = 7;
+            // 
+            // label8
+            // 
+            label8.AutoSize = true;
+            label8.Font = new Font("Segoe UI", 9F, FontStyle.Bold, GraphicsUnit.Point);
+            label8.ForeColor = Color.DarkCyan;
+            label8.Location = new Point(23, 214);
+            label8.Name = "label8";
+            label8.Size = new Size(45, 15);
+            label8.TabIndex = 18;
+            label8.Text = "Precio:";
+            // 
+            // maskedTextBox3
+            // 
+            maskedTextBox3.Location = new Point(146, 250);
+            maskedTextBox3.Name = "maskedTextBox3";
+            maskedTextBox3.Size = new Size(164, 23);
+            maskedTextBox3.TabIndex = 8;
+            // 
+            // label9
+            // 
+            label9.AutoSize = true;
+            label9.Font = new Font("Segoe UI", 9F, FontStyle.Bold, GraphicsUnit.Point);
+            label9.ForeColor = Color.DarkCyan;
+            label9.Location = new Point(23, 253);
+            label9.Name = "label9";
+            label9.Size = new Size(90, 15);
+            label9.TabIndex = 19;
+            label9.Text = "Stock Máximo:";
+            // 
+            // maskedTextBox4
+            // 
+            maskedTextBox4.Location = new Point(146, 288);
+            maskedTextBox4.Name = "maskedTextBox4";
+            maskedTextBox4.Size = new Size(164, 23);
+            maskedTextBox4.TabIndex = 9;
+            // 
+            // label10
+            // 
+            label10.AutoSize = true;
+            label10.Font = new Font("Segoe UI", 9F, FontStyle.Bold, GraphicsUnit.Point);
+            label10.ForeColor = Color.DarkCyan;
+            label10.Location = new Point(23, 291);
+            label10.Name = "label10";
+            label10.Size = new Size(87, 15);
+            label10.TabIndex = 20;
+            label10.Text = "Stock Mínimo:";
+            // 
+            // maskedTextBox5
+            // 
+            maskedTextBox5.Location = new Point(146, 328);
+            maskedTextBox5.Name = "maskedTextBox5";
+            maskedTextBox5.Size = new Size(164, 23);
+            maskedTextBox5.TabIndex = 10;
+            // 
+            // label11
+            // 
+            label11.AutoSize = true;
+            label11.Font = new Font("Segoe UI", 9F, FontStyle.Bold, GraphicsUnit.Point);
+            label11.ForeColor = Color.DarkCyan;
+            label11.Location = new Point(21, 331);
+            label11.Name = "label11";
+            label11.Size = new Size(103, 15);
+            label11.TabIndex = 0;
+            label11.Text = "Código de Barras:";
+            // 
+            // button3
+            // 
+            button3.BackColor = Color.DarkCyan;
+            button3.FlatStyle = FlatStyle.Flat;
+            button3.Font = new Font("Segoe UI", 9F, FontStyle.Bold, GraphicsUnit.Point);
+            button3.ForeColor = SystemColors.Control;
+            button3.Location = new Point(593, 18);
+            button3.Name = "button3";
+            button3.Size = new Size(85, 27);
+            button3.TabIndex = 3;
+            button3.Text = "Limpiar";
+            button3.UseVisualStyleBackColor = false;
+            // 
+            // checkBox1
+            // 
+            checkBox1.AutoSize = true;
+            checkBox1.CheckAlign = ContentAlignment.MiddleRight;
+            checkBox1.FlatStyle = FlatStyle.Flat;
+            checkBox1.Font = new Font("Segoe UI", 9F, FontStyle.Bold, GraphicsUnit.Point);
+            checkBox1.ForeColor = Color.DarkCyan;
+            checkBox1.Location = new Point(726, 332);
+            checkBox1.Name = "checkBox1";
+            checkBox1.Size = new Size(59, 19);
+            checkBox1.TabIndex = 11;
+            checkBox1.Text = "Activo";
+            checkBox1.TextAlign = ContentAlignment.MiddleCenter;
+            checkBox1.UseVisualStyleBackColor = true;
+            // 
+            // Articulos
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = Color.Azure;
+            ClientSize = new Size(833, 507);
+            Controls.Add(panel2);
+            Controls.Add(panel1);
+            MaximizeBox = false;
+            MinimizeBox = false;
+            Name = "Articulos";
+            Text = "Artículos";
+            panel1.ResumeLayout(false);
+            panel2.ResumeLayout(false);
+            panel2.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private Button button1;
+        private Panel panel1;
+        private Button button5;
+        private Button button4;
+        private Button button2;
+        private Panel panel2;
+        private Label label1;
+        private PictureBox pictureBox1;
+        private ComboBox comboBox1;
+        private Label label3;
+        private Label label2;
+        private Label label7;
+        private Label label6;
+        private ComboBox comboBox3;
+        private ComboBox comboBox2;
+        private Label label5;
+        private TextBox textBox1;
+        private Label label4;
+        private MaskedTextBox maskedTextBox1;
+        private MaskedTextBox maskedTextBox4;
+        private Label label10;
+        private MaskedTextBox maskedTextBox3;
+        private Label label9;
+        private MaskedTextBox maskedTextBox2;
+        private Label label8;
+        private MaskedTextBox maskedTextBox5;
+        private Label label11;
+        private Button button3;
+        private CheckBox checkBox1;
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,3 +1,4 @@
+using Farmaceutica.Presentacion;
 namespace Farmaceutica
 {
     internal static class Program
@@ -11,7 +12,7 @@ namespace Farmaceutica
             // To customize application configuration such as set high DPI settings or default font,
             // see https://aka.ms/applicationconfiguration.
             ApplicationConfiguration.Initialize();
-            Application.Run(new Form1());
+            Application.Run(new Articulos());
         }
     }
 }
