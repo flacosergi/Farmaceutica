97c48d842c243e00a8840e21dc38e958179abae3
galianosergio <galianosergio@hotmail.com>
2023-10-18 18:01:18 -0300

    sg

--- Presentacion/Articulos.cs
+++ Presentacion/Articulos.cs
@@ -3,18 +3,52 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
+using System.Security.Policy;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using static Farmaceutica.Program;
+
 
 namespace Farmaceutica.Presentacion
 {
     public partial class Articulos : Form
     {
+        List<WeatherForecast> lista = new List<WeatherForecast>();
+
         public Articulos()
         {
             InitializeComponent();
+
+        }
+
+        private async void button1_Click(object sender, EventArgs e)
+        {
+            lista = await GetProductAsync(url.PathAndQuery);
+        }
+
+        private async Task<List<WeatherForecast>> GetProductAsync(string path)
+        {
+            
+            List<WeatherForecast> lista = new List<WeatherForecast>();
+            HttpResponseMessage response = await client.GetAsync(path);
+            if (response.IsSuccessStatusCode)
+            {
+                lista = await response.Content.ReadAsAsync<List<WeatherForecast>>();
+            }
+            return lista;
         }
     }
+
+    public class WeatherForecast
+    {
+        public DateTime Date { get; set; }
+        public int TemperatureC { get; set; }
+        public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+        public string? Summary { get; set; }
+    }
+
+
 }
--- Program.cs
+++ Program.cs
@@ -1,18 +1,26 @@
 using Farmaceutica.Presentacion;
+using System.Net.Http.Headers;
+
 namespace Farmaceutica
 {
-    internal static class Program
+    public static class Program
     {
         /// <summary>
         ///  The main entry point for the application.
         /// </summary>
+        public static HttpClient client = new HttpClient();
         [STAThread]
+
         static void Main()
         {
             // To customize application configuration such as set high DPI settings or default font,
             // see https://aka.ms/applicationconfiguration.
             ApplicationConfiguration.Initialize();
+            client.BaseAddress = new Uri("http://localhost:64195/");
+            client.DefaultRequestHeaders.Accept.Clear();
+            client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
             Application.Run(new Articulos());
         }
+
     }
 }
