7b2c37245d3c2036c84d82f0661f6f22022a4b46
¨AgusGattas¨ <agusgattasrp@gmail.com>
2023-10-26 16:34:43 -0300

    Se agrega el formulario principal

---
+++ Presentacion/FrmPrincipal.cs
@@ -0,0 +1,44 @@
+﻿using AccesoDatos.Servicios;
+using Farmaceutica.Servicios;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Farmaceutica.Presentacion
+{
+    public partial class FrmPrincipal : Form
+    {
+        ServiciosFactory factory = new ServiciosFactory();
+        public FrmPrincipal()
+        {
+            InitializeComponent();
+
+        }
+
+        private void Principal_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void salirToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+
+            if (MessageBox.Show("Seguro que desea salir?", "Confirmación", MessageBoxButtons.YesNo, MessageBoxIcon.Question) == DialogResult.Yes)
+            {
+                this.Close();
+            }
+        }
+
+        private void agregarArticulosToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            FrmArticulos articulos = new FrmArticulos(factory);
+            articulos.ShowDialog();
+        }
+    }
+}
